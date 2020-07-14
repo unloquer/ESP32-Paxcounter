c016954952b7ff4e872728a16dc0e8122f605a93
Julian Giraldo <julian.giraldo.h@gmail.com>
2020-07-14 10:14:02 -0500

    Agrega funcionalidad del display, es necesario adaptar a un display de 64x48 pxs como el que estamos usando

--- src/hal/lolin32lite.h
+++ src/hal/lolin32lite.h
@@ -1,5 +1,5 @@
 // clang-format off
-// upload_speed 921600
+// upload_speed 115200
 // board lolin32
 
 #ifndef _LOLINLITE_H
@@ -8,12 +8,15 @@
 #include <stdint.h>
 
 // Hardware related definitions for lolin32lite (without LoRa shield)
-
+#define HAS_DISPLAY 1
 #define HAS_LED LED_BUILTIN // on board LED on GPIO5
 #define LED_ACTIVE_LOW 1    // Onboard LED is active when pin is LOW
 
-#define MY_SDA SDA
-#define MY_SCL SCL
+/* #define MY_SDA SDA //21 */
+/* #define MY_SCL SCL //22 */
+#define MY_DISPLAY_SDA (21)
+#define MY_DISPLAY_SCL (22)
+#define MY_DISPLAY_RST NOT_A_PIN
 
 // disable brownout detection (avoid unexpected reset on some boards)
 #define DISABLE_BROWNOUT 1 // comment out if you want to keep brownout feature
